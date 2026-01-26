7bb220a8c868c4308155c7726ba4f916b1d8724c
erik_does_research <erik.mamet@gmail.com>
2026-01-26 05:15:57 -0500

    added a setup.py and modified gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SOGS",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Wieland Morgenstern and Florian Barthel and Anna Hilsmann and Peter Eisert (Main implementation of static SOGS) + Erik Mamet (playground)",
+    description="Modified Dynamic 3D Gaussians implementation from https://github.com/JonathonLuiten/Dynamic3DGaussians",
+)
